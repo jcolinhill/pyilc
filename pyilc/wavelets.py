261093801144ba96ab7a83d2a2fc4daaaa178413
Fiona McCarthy <fmccarthy@popeye-login2.cm.cluster>
2023-05-15 20:39:15 -0700

    more vectorization

--- pyilc/wavelets.py
+++ pyilc/wavelets.py
@@ -529,10 +529,14 @@ def wavelet_ILC(wv=None, info=None, ILC_bias_tol=1.e-3, wavelet_beam_criterion=1
                 else:
                     for a in range(N_comps):
                         QSa_temp = np.delete(np.delete(Qab_pix, a, 0), 0, 1) #remove the a^th row and zero^th column
-                        tempvec[a] = (-1.0)**float(a) * np.array([np.linalg.det(QSa_temp[:,:,p]) for p in range(int(N_pix_to_use[j]))])
+                        # Fiona edit: vectorizing
+                        #tempvec[a] = (-1.0)**float(a) * np.array([np.linalg.det(QSa_temp[:,:,p]) for p in range(int(N_pix_to_use[j]))])
+                        tempvec[a] = (-1.0)**float(a) * np.linalg.det(np.transpose(QSa_temp,(2,0,1))) 
                 tmp2 = np.einsum('ia,ap->ip', A_mix, tempvec)
                 tmp3 = np.einsum('jip,ip->jp', inv_covmat_temp, tmp2)
-                weights = np.array([(1.0 / np.linalg.det(Qab_pix[:,:,p]))*tmp3[:,p] for p in range(int(N_pix_to_use[j]))]) #N.B. 'weights' here only includes channels that passed beam_thresh criterion, todo: parallelize
+                # Fiona edit: vectorise
+                weights = 1.0/np.linalg.det(np.transpose(Qab_pix,(2,0,1)))[:,None]*np.transpose(tmp3) #N.B. 'weights' here only includes channels that passed beam_thresh criterion, todo: parallelize
+                # weights = np.array([(1.0 / np.linalg.det(Qab_pix[:,:,p]))*tmp3[:,p] for p in range(int(N_pix_to_use[j]))]) #N.B. 'weights' here only includes channels that passed beam_thresh criterion, todo: parallelize
                 # response verification
                 response = np.einsum('pi,ia->ap', weights, A_mix) #dimensions N_comps x N_pix_to_use[j]
                 optimal_response_preserved_comp = np.ones(int(N_pix_to_use[j]))  #preserved component, want response=1
