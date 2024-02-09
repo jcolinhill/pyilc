c7f4ef01393a5d50bfcf148a609742c74fadb89a
fmccarthy <43215980+fmccarthy@users.noreply.github.com>
2024-02-09 18:32:00 -0500

    Update input.py

--- pyilc/input.py
+++ pyilc/input.py
@@ -310,6 +310,7 @@ class ILCInfo(object):
         # by default we don't do this, but if you want to do this put this here
         # this should be a list with N_scales entries, eg if you want to subtract the means on the realspace domains
         # on some scales and not others.
+        # THIS REMOVES SIGNAL DON'T DO THIS UNLESS YOU KNOW WHAT IT IS DOING
         self.subtract_means_before_sums = [False] * self.N_scales
         if 'subtract_means_before_sums' in p.keys():
             self.subtract_means_before_sums = p['subtract_means_before_sums']
